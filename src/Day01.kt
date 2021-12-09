80e2a3a8f793d84e9871e80c11a83c078f21e891
astamato <alejandra.stamato@gmail.com>
2021-12-09 00:35:39 +0000

    Advent of Code day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,45 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var count = 0
+        var isFirst = true
+        var previous = -1
+        input.forEach { current ->
+            val currentCast = current.toInt()
+            if (!isFirst) {
+                if (currentCast > previous) count++
+                previous = currentCast
+            } else {
+                isFirst = false
+            }
+        }
+        return count
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var count = 0
+        var i = 1
+        var previous: List<Int> = input.subList(0, 3).map { it.toInt() }
+
+        while (i + 3 <= input.size) {
+            val currentTarget = input.subList(i, i + 3).toList()
+            if (currentTarget.size == 3) {
+                val current = currentTarget.map { it.toInt() }.toList()
+                var currentSum = 0
+                current.forEach { currentSum += it }
+
+                var previousSum = 0
+                previous.forEach { previousSum += it }
+
+                if (currentSum > previousSum) count++
+
+                previous = current
+            }
+            i += 1
+        }
+        return count
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
+    val input = readInput("Day01_test")
     println(part1(input))
     println(part2(input))
 }
